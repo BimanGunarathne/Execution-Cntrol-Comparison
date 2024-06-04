ac93ca784293bfe16f400ac12720396b0c077ba2
Biman Gunarathne <bimangunarathne@outlook.com>
2024-06-04 14:07:24 +0530

    updated

--- example.java
+++ example.java
@@ -16,5 +16,12 @@ public class example {
             int num2 = 9*6;
             System.out.println(num2);
         }
+        // ----------------------------------------
+        int z = 0;
+        if (z == 0) {
+            System.out.println(1.1);
+        } else {
+            System.out.println(-1.1);
+        }
     }
 }
