620f6e88cf0fee18dcb9dc9c41d38b3eeb628742
Biman Gunarathne <bimangunarathne@outlook.com>
2024-06-04 09:17:02 +0530

    create a q nad java file to compare execution

---
+++ example.java
@@ -0,0 +1,12 @@
+public class example {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        if (true) {
+            int num1 = 42;
+            System.out.println(num1);
+        } else {
+            int num2 = 9*6;
+            System.out.println(num2);
+        }
+    }
+}
